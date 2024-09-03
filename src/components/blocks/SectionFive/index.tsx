17c948bfbceb800efade8a46ce7e00ec9bd7e7c9
Luke Thompson <luke.thompson@nine.com.au>
2024-09-03 17:28:22 +1000

    changes

--- src/components/blocks/SectionFive/index.tsx
+++ src/components/blocks/SectionFive/index.tsx
@@ -3,7 +3,7 @@ import backgroundImg from "../../../assets/images/SecFive-bg.png";
 import backgroundImgMobile from "../../../assets/images/SecFive-bg-mobile.png";
 import image15 from "../../../assets/images/15.jpg";
 import image16 from "../../../assets/images/16.jpg";
-import image17 from "../../../assets/images/SecFive-dec.png";
+import image17 from "../../../assets/images/secFive-dec.png";
 import { motion } from "framer-motion";
 
 const SectionFive: React.FC = () => (
