f0bd4420e1ce51e643637f028be20c054499e78e
Luke Thompson <luke.thompson@nine.com.au>
2024-09-04 16:27:47 +1000

    corrected types for MotionImageProps

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -32,12 +32,12 @@ export type MotionImageProps = {
   alt: string
   "data-sccroll"?: boolean
   "data-scroll-speed"?: string
-  initial?: { opacity?: number; rotate?: string; scale?: number; y?: string }
+  initial?: { opacity?: number; rotate?: string; scale?: number; y?: number }
   whileInView?: {
     opacity?: number
     rotate?: string
     scale?: number
-    y?: string
+    y?: number
   }
   transition?: { duration: number; delay: number; ease: string }
   className?: string
